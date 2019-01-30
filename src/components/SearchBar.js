89ddf0c00cfc5920896b22fbf59aabd27cee7af8
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-30 14:06:12 -0700

    create state and give value to input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
   render() {
     return (
-        //show at least a form element, label and input
       <div className='search-bar ui segment'>
         <form className='ui form'>
           <div className='field'>
             <label>Video Search</label>
-            <input type='text' />
+            //turn input from uncontrolled to a controlled input
+            //hook it to State of SearchBar class, store data(term) inside component and NOT the DOM
+            <input type='text' value={this.state.term} />
           </div>
         </form>
       </div>
