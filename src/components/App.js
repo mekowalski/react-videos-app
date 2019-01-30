320fed0047a7aad0d14261e271e07866ea8ccccb
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-30 15:20:06 -0700

    call onTermSubmit with axios preconfig

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    console.log(term)
+  onTermSubmit = term => {
+    //pre-configured instance of axios
+    //and can now pass in the params of q
+    //will need to use a promise or async await with this request
+    youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
   }
 
   render() {
