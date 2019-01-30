7d0940608046886a7cc6beb1fde841ec7bd568a6
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-30 13:33:43 -0700

    create components DIR, create and add App.js file, create App class comp

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+//App will be class-based component because it will hold all the different state for application
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <div>This is the Videos App</div>;
+  }
+}
+
+export default App
