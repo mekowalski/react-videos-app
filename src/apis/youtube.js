47daf30e380af28d6d3c3355c5a0ca16316afc73
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-30 14:38:41 -0700

    explain user accessing API KEY

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,3 +1,7 @@
 const KEY = 'AIzaSyAJPpjtCVPSOF-Q-KIWR3Zjzusl_XYUflc'
 
 //cap KEY: this is a constant value that should not be changed
+//This KEY will be used inside the browser
+//When a user accesses this applciation, they will be sent this API KEY
+//The user could potentially get hands on the API KEY
+//This specific API KEY is MEANT to be publicly available, meant for public access
