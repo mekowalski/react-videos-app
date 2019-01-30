098f514e773e85d81c576f073692dd5ea3b459c1
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-30 14:50:40 -0700

    add notes on API KEY and YT api docs

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -5,3 +5,7 @@ const KEY = 'AIzaSyAJPpjtCVPSOF-Q-KIWR3Zjzusl_XYUflc'
 //When a user accesses this applciation, they will be sent this API KEY
 //The user could potentially get hands on the API KEY
 //This specific API KEY is MEANT to be publicly available, meant for public access
+
+//Google allows API Restrictions for HTTP referrers
+//Once selected HTTP referrers, add localhost:3000 and save
+//Only those accessing application from localhost:3000 will be able tosee/use the app
