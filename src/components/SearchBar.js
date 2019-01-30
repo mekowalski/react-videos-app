06365ec6452b899f0792b927e9c38926b19b4ec3
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-30 14:14:09 -0700

    onChange Prop for input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  //this is an event callback() therefore define with ES6 =>()
+  onInputChange = () => {
+
+  }
+
   render() {
     return (
       <div className='search-bar ui segment'>
@@ -11,7 +16,11 @@ class SearchBar extends React.Component {
             <label>Video Search</label>
             //turn input from uncontrolled to a controlled input
             //hook it to State of SearchBar class, store data(term) inside component and NOT the DOM
-            <input type='text' value={this.state.term} />
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
