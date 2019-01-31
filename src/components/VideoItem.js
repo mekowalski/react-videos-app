2e187462af55d7c1d26db2def9c0958203908ccd
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 13:23:38 -0700

    destructure and pass onVideoSlect as VideoItem prop, create event handler to use onVideoSelect on click

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,12 @@
 import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+//destructure cb()
+//add event handler
+//call cb() with the video that is selected
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className='video-item item'>
+    <div onClick={() => onVideoSelect(video)} className='video-item item'>
       <img className='ui image' src={video.snippet.thumbnails.medium.url} />
       <div className='content'>
         <div className='header'>{video.snippet.title}</div>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+//destructure onVideoSelect, that is a cb() from App to VideoList
+//pass cb() onto VideoItem
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   })
   return (
     <div className='ui relaxed divided list'>
