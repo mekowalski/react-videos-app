871e749fc6bf2bc733abe75a1c87027642923a0a
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 14:24:59 -0700

    create VideoDetail comp, import in App and call with an added prop of video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,1 +1,10 @@
 //responsible for showing video player, title and description
+import React from 'react';
+
+//destructure video title from props
+//props.video => video
+const VideoDetail = ({ video }) => {
+  return <div>{video.snippet.title}</div>
+}
+
+export default VideoDetail
