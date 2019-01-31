41b283f7d2e9bf3914f3390f750d0e147ae23f21
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 11:08:48 -0700

    add async await and log response

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,17 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    console.log(response)
   }
+  //asynchronous operation needs either promises or async await
+  //console.log returns a data property, that contains the actual info, then retrieve the arrays property
 
   render() {
     return (
