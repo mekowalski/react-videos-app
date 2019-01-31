611f51c682071b42cb6e29ee956d095eae374ae1
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 14:52:34 -0700

    create videoSrc variable, call the iframe src, video player displays the vidoe!!!

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,15 +5,17 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   //ui embed is a semantic stating that an embedded video player is about to be inside
   //how to pass iframe its src
   //Network-Previews items => videos => id => videoId gives specific id to each video
   //video.id.videoId
   return (
     <div>
-    <div className='ui embed'>
-      <iframe src={} />
-    <div/>
+      <div className='ui embed'>
+        <iframe src={videoSrc} />
+      </div>
       <div className='ui segment'>
         <h4 className='ui header'>{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
