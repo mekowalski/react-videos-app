b02309e1fd08d7ff193474edb3889d55888b73f9
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 12:43:03 -0700

    add css class to div tag, add custome styling, correctly displays

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const VideoItem = ({ video }) => {
   return (
-    <div className='item'>
+    <div className='video-item item'>
       <img className='ui image' src={video.snippet.thumbnails.medium.url} />
       <div className='content'>
         <div className='header'>{video.snippet.title}</div>
