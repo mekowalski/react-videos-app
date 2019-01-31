35cc0a0026d5d22521062bdf1d856190493bfd0b
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 11:14:05 -0700

    state and setState

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,24 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  //first default state to be an emptry array, because this return is expected to be an array in the future
+  state = { videos: [] };
+
+  //after request and return is made, video.length will be updated
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-
-    console.log(response)
+    this.setState({ videos: response.data.items })
   }
-  //asynchronous operation needs either promises or async await
-  //console.log returns a data property, that contains the actual info, then retrieve the arrays property
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos
       </div>
     );
   }
