c1b5a1fa2a1f2dbefa04316f9d056a40191cda51
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-31 11:22:40 -0700

    import VideoList and call in in App and pass videos props

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   //first default state to be an emptry array, because this return is expected to be an array in the future
@@ -20,7 +21,9 @@ class App extends React.Component {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos
+        <VideoList videos={this.state.videos} />
+        //When APP shows VideoList, VideoList has to be given the list of video objects in order
+        //to render on screen
       </div>
     );
   }
