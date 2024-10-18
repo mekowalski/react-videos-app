bb6e5b9b491adf1fd9b38eb2c6a5bdc2f5f8a6b2
Malind Kowalski <malindkowalski@gmail.com>
2024-10-17 21:17:06 -0600

    trying to resolve 403/400 error, updated key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAJPpjtCVPSOF-Q-KIWR3Zjzusl_XYUflc'
+const KEY = 'AIzaSyCWx3mJk-T5vbNnhXOm03bX5u06Ral0NMQ'
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
